644288ac6020b5f38edc1445de39d72e7eba1796
Ilyas Ronef <ilyas@divandesign.biz>
2015-11-16 21:05:28 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# ddDocsSubdomainsUrlConverter
+При генерации URL средставами MODX изменяет их таким образом, чтобы корневая папка с необходимым шаблоном становилась поддоменом.
+
+## Requires
+1. MODXEvo >= 1.0.15_3595d8b791d0dc31ce7c08876c0c0c0a342c73fe
+2. MODXEvo.library.ddTools >= 0.14.2
+
+## Events
+* OnMakeDocUrl
+
+## Config
+`&subdomainDocTemplateId=Template id of subdomain documents;text;`
+
+## Parameters description
+* $subdomainDocTemplateId {integer} — ID шаблона корневой папки-поддомена.
+
+## Usage
+Пусть структура документов будет такая:
+
+* Home (1)
+* About (2)
+* Blog (3)
+	* Article (6)
+* _De (4)_
+	* Home (7)
+	* About (8)
+	* Blog (9)
+		* Article (12)
+* _Fr (5)_
+	* Home (10)
+	* About (11)
+
+Для документов «De (4)» и «Fr (5)» понадобится отдельный шаблон, назовём его «Subdomain template» (пусть его ID = «4»).
+
+_Параметр плагина `$subdomainDocTemplateId`, соответственно, выставляем в `4`._
+
+### Когда находимся на одной из страниц основного домена «domain.com»
+Адреса страниц при генерации будут преобразованы следующим образом:
+* `[~9~]`: `/de/blog` → `http://de.domain.com/blog` (т. к. ссылка на страницу другого домена, она должна быть абсолютной).
+* `[~3~]`: `/blog` → `/blog` (ссылка на страницу в рамках текущего домена, так что останется без изменений).
+* `[~11~]`: `/fr/about` → `http://fr.domain.com/about`.
+
+Абсолютные ссылки преобразуются аналогичным образом:
+* `http://domain.com/de/about` → `http://de.domain.com/about`.
+* `http://domain.com/blog` → `http://domain.com/blog`.
+
+### Когда находимся на одной из страниц поддомена «de.domain.com»
+* `[~8~]`: `/de/about` → `/about` (внутренняя, т. к. находимся в рамках текущего домена).
+* `[~9~]`: `/de/blog` → `/blog`.
+* `[~2~]`: `/about` → `http://domain.com/about` (теперь на основной домен необходимо строить внешние ссылки).
+* `[~3~]`: `/blog` → `http://domain.com/blog`.
+* `[~11~]`: `/fr/about` → `http://fr.domain.com/about` (тоже внешняя).
+
+_Поддомен «www.domain.com» обрабатывается так же, как основной «domain.com»._
+
+## Changelog
+### Version 1.0 (2015-02-22)
+* \+ The first release.
---
+++ ddDocsSubdomainsUrlConverter.php
@@ -0,0 +1,85 @@
+//<?php
+/**
+ * ddDocsSubdomainsUrlConverter.php
+ * @version 1.0 (2015-02-22)
+ * 
+ * @desc При генерации URL средставами MODX изменяет их таким образом, чтобы корневая папка с необходимым шаблоном становилась поддоменом («http://domain.com/de/about» → «http://de.domain.com/about» или просто «about», если находимся в рамках поддомена).
+ * 
+ * @uses MODXEvo >= 1.0.15_3595d8b791d0dc31ce7c08876c0c0c0a342c73fe.
+ * @uses The library modx.ddTools 0.14.2.
+ * 
+ * @param $subdomainDocTemplateId {integer} — ID шаблона корневой папки-поддомена. @required
+ * 
+ * @config &subdomainDocTemplateId=Template id of subdomain documents;text;
+ * @event OnMakeDocUrl
+ * 
+ * @copyright 2015 DivanDesign {@link http://www.DivanDesign.biz }
+ */
+
+if (!isset($subdomainDocTemplateId) || !is_numeric($subdomainDocTemplateId)){return;}
+
+if ($modx->Event->name == 'OnMakeDocUrl'){
+	//Подключаем modx.ddTools
+	require_once $modx->getConfig('base_path').'assets/snippets/ddTools/modx.ddtools.class.php';
+	
+	//Получаем корневого родителя
+	$rootParent = $modx->getParentIds($modx->Event->params['id']);
+	
+	if (count($rootParent) > 0){
+		$rootParent = array_pop($rootParent);
+	}else{
+		$rootParent = $modx->Event->params['id'];
+	}
+	
+	//Нам нужен его шаблон и псеводним
+	$rootParent = ddTools::getDocument($rootParent, 'template,alias');
+	
+	//Псевдоним текущего поддомена (для основного домена будет == 'www')
+	$currentSubdomainAlias = array_reverse(explode('.', $_SERVER['HTTP_HOST']));
+	//Если домена третьего уровня нет (мало ли), значит мы основном домене, иначе — на поддомене
+	$currentSubdomainAlias = !isset($currentSubdomainAlias[2]) ? 'www' : $currentSubdomainAlias[2];
+	
+	//Разберём url текущего документа
+	$url = parse_url($modx->Event->params['url']);
+	
+	//Допишем недостающий слэш вначале пути при необходимости
+	if (substr($url['path'], 0, 1) != '/'){$url['path'] = '/'.$url['path'];}
+	
+	//По умолчанию считаем, что строим ссылку на страницу без поддомена (ну, а какая разница?)
+	$buildSubdomainAlias = 'www';
+	
+	//Если ссылка формируется на одну из страниц в папке-поддомена
+	if ($rootParent['template'] == $subdomainDocTemplateId){
+		$buildSubdomainAlias = $rootParent['alias'];
+		
+		//Убираем псевдоним папки-поддомена из начала пути
+		$url['path'] = substr($url['path'], strlen('/'.$buildSubdomainAlias));
+	}
+	
+	//Если мы сейчас не на поддомене, на страницу которого строим ссылку, то нужен полный URL (внешняя ссылка)
+	if ($currentSubdomainAlias != $buildSubdomainAlias){
+		//Добавляем схему при необходимости
+		if (!isset($url['scheme'])){$url['scheme'] = !isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == 'off' ? 'http' : 'https';}
+		//Добавляем хост при необходимости
+		if (!isset($url['host'])){$url['host'] = $_SERVER['HTTP_HOST'];}
+		
+		//Разбиваем на домены разных уровней домены
+		$url['host'] = array_reverse(explode('.', $url['host']));
+		//Нам нужны только 3 уровня доменов (ну мало ли)
+		$url['host'] = array_slice($url['host'], 0, 3);
+		//Доменом третьего уровня будет необходимый поддомен (как удобно, что у основного будет 'www')
+		$url['host'][2] = $buildSubdomainAlias;
+		//Склеиваем обратно
+		$url['host'] = implode('.', array_reverse($url['host']));
+	}
+	
+	//Собираем URL обратно (очень в упрощённом виде конечно). Было бы здорово использовать «http_build_url», но не везде есть.
+	$url['scheme'] = isset($url['scheme']) ? $url['scheme'].'://' : '';
+	$url['host'] = isset($url['host']) ? $url['host'] : '';
+	$url['query'] = isset($url['query']) ? '?'.$url['query'] : '';
+	
+	$url = $url['scheme'].$url['host'].$url['path'].$url['query'];
+	
+	$modx->Event->output($url);
+}
+//?>
